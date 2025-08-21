4f513cc1459a5cc9faeafe8e65c72697091c46e2
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-08-21 13:53:24 +0200

    style: replace tailwindcss with primeflexcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'PrimeCards',
+      htmlAttrs: {
+        lang: 'en'
+      }
+    }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase', '@pinia/nuxt', '@primevue/nuxt-module', '@nuxtjs/tailwindcss'],
+  css: ['primeflex/primeflex.css'],
+  modules: ['@nuxtjs/supabase', '@pinia/nuxt', '@primevue/nuxt-module'],
   supabase: {
     url: process.env.NUXT_PUBLIC_SUPABASE_URL,
     key: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY,
