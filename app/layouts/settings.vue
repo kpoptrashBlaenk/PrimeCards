d67b33ca8304f6dc7e2fa621e10e0a01729bac10
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-09-08 13:46:23 +0200

    feat(profile): add settings sidebar layout

---
+++ app/layouts/settings.vue
@@ -0,0 +1,12 @@
+<template>
+  <NavbarMenubar />
+  <div class="grid container">
+    <div class="col-12 md:col-4 md:px-8">
+      <SettingsSidebar />
+    </div>
+
+    <div class="col-12 md:col-8">
+      <slot></slot>
+    </div>
+  </div>
+</template>
--- app/pages/settings/account.vue
+++ app/pages/settings/account.vue
@@ -1,3 +1,16 @@
-<template></template>
+<template>
+  <SettingsPage />
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+/* Import */
+import authGlobal from '@middleware/auth.global'
+
+/* Import */
+
+/* PageMeta */
+definePageMeta({
+  layout: 'settings',
+  middleware: authGlobal,
+})
+</script>
