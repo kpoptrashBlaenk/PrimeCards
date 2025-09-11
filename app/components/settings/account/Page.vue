3c852a581f7fba2db7376ab083489c0cac7be820
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-09-11 15:58:33 +0200

    style(profile): settings now an accordion

--- app/components/settings/account/Page.vue
+++ app/components/settings/account/Page.vue
@@ -2,16 +2,33 @@
   <div>
     <SettingsTitle title="Account" context="title" />
     <Divider />
-    <SettingsTitle title="Avatar" context="subtitle" :first="true" />
-    <SettingsAccountAvatar />
-    <SettingsTitle title="General" context="subtitle" />
-    <SettingsAccountForm />
-    <SettingsTitle title="Change Email" context="subtitle" />
-    <SettingsAccountEmailForm />
-    <SettingsTitle title="Change Password" context="subtitle" />
-    <SettingsAccountPasswordForm />
-    <SettingsTitle title="Delete Account" context="subtitle" />
-    <SettingsAccountDelete />
+
+    <Accordion value="0">
+      <AccordionPanel v-for="(tab, index) in tabs" :value="index">
+        <AccordionHeader class="bg-transparent">
+          <SettingsTitle :title="tab.title" context="subtitle" :first="index === 0" />
+        </AccordionHeader>
+        <AccordionContent :pt="{ content: { class: 'bg-transparent' } }">
+          <SettingsAccountAvatar v-if="index === 0" />
+          <SettingsAccountForm v-else-if="index === 1" />
+          <SettingsAccountEmailForm v-else-if="index === 2" />
+          <SettingsAccountPasswordForm v-else-if="index === 3" />
+          <SettingsAccountDelete v-else-if="index === 4" />
+        </AccordionContent>
+      </AccordionPanel>
+    </Accordion>
+
     <Toast />
   </div>
 </template>
+
+<script setup lang="ts">
+/* Refs */
+const tabs = ref([
+  { title: 'Avatar' },
+  { title: 'General' },
+  { title: 'Change Email' },
+  { title: 'Change Password' },
+  { title: 'Delete Account' },
+])
+</script>
