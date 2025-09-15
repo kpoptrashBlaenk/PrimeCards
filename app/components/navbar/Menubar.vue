b94c42e755e665f499f63bb447f3eacc34920e5b
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-09-15 09:56:34 +0200

    feat: show page name in navbar

--- app/components/navbar/Menubar.vue
+++ app/components/navbar/Menubar.vue
@@ -2,12 +2,18 @@
   <Menubar>
     <!-- Start -->
     <template #start>
-      <NuxtLink class="text-2xl font-bold no-underline text-logo" :to="'/'"> PrimeCards </NuxtLink>
+      <div class="flex align-items-baseline gap-2">
+        <NuxtLink class="text-2xl font-bold no-underline text-logo" :to="'/'"> PrimeCards </NuxtLink>
+      </div>
     </template>
 
     <!-- End -->
     <template #end>
-      <NavbarProfileMenu v-if="userStore.user" />
+      <div v-if="userStore.user" class="flex align-items-center gap-3">
+        <div class="font-bold text-md">{{ menuTitle }}</div>
+        <NavbarProfileMenu />
+      </div>
+
       <NuxtLink v-else :to="'/auth/login'">
         <Button type="submit" class="font-bold text-outline">Sign In</Button>
       </NuxtLink>
@@ -17,6 +23,16 @@
 <script setup lang="ts">
 /* Imports */
 import { useUserStore } from '@stores/user'
+
+/* Router */
+const route = useRoute()
+
+/* Computeds */
+const menuTitle = computed(() => {
+  const segments = route.path.split('/').filter(Boolean)
+  const first = segments[0] || ''
+  return first.charAt(0).toUpperCase() + first.slice(1)
+})
 
 /* Stores */
 const userStore = useUserStore()
